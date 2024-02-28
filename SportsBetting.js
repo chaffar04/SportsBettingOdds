668edd630716c8a7b6d1814f69ccb31dd1310cd0
carlhaffar <chaffar@uwo.ca>
2024-02-28 18:37:14 -0500

    commit

--- SportsBetting.js
+++ SportsBetting.js
@@ -1,16 +1,70 @@
 const axios = require("axios");
 
-async function fetchData() {
-  const API_KEY = "ae91e974d42ed39b3993d3eed96cbf8e";
+const API_KEY = "ae91e974d42ed39b3993d3eed96cbf8e";
 
+async function getLeagues(apiKey) {
   try {
+    let leagueDict = {};
     const response = await axios.get(
-      `https://api.the-odds-api.com/v4/sports/basketball_ncaab/odds/?apiKey=${API_KEY}&regions=us,us2,uk,au,eu&markets=h2h`
+      `https://api.the-odds-api.com/v4/sports/?apiKey=${apiKey}`
     );
-    console.log(response.data);
+    response.data.forEach((league) => {
+      leagueDict[league.key] = null;
+    });
+    return leagueDict;
   } catch (error) {
     console.error("Error:", error);
   }
 }
 
-fetchData();
+async function getGames(apiKey) {
+  try {
+    const leagues = await getLeagues(apiKey);
+
+    for (const leagueKey in leagues) {
+      const response = await axios.get(
+        `https://api.the-odds-api.com/v4/sports/${leagueKey}/odds/?apiKey=${apiKey}&regions=us&markets=h2h`
+      );
+      response.date.forEach((game) => {});
+      console.log(response.data);
+    }
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+function getGameDetails(game) {
+  bookmaker = league = game.sport_title;
+}
+
+function decimalOdd(odds) {
+  if (odds.length < 3) {
+    return 1 - Math.pow(odds[0], -1) - Math.pow(odds[1], -1);
+  } else {
+    //draw
+  }
+}
+
+function drawDecimalOdd(odd) {}
+
+class Game {
+  constructor(bookmaker, league, teams, odds) {
+    this.bookmaker = bookmaker;
+    this.league = league;
+    this.teams = teams;
+    this.odds = odds;
+  }
+
+  interestRate() {
+    let temp;
+    if (this.odds.length < 3) {
+      odds.forEach((odd) => {
+        temp += decimalOdd(odd);
+      });
+      return 1 - temp;
+    } else {
+    }
+  }
+}
+
+getGames(API_KEY);
